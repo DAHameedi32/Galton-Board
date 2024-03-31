a1648dde2d8a628646ca5a9f0dd04cc90683534f
DAHameedi32 <daniyal.hameedi@kcl.ac.uk>
2024-03-31 19:07:59 +0100

    commit 31/03/2024

--- Assets/Grid_Generator.cs
+++ Assets/Grid_Generator.cs
@@ -8,8 +8,12 @@ public class Grid_Generator : MonoBehaviour
     public GameObject peg;
     public float X;
 
+    float AltX = X-1;
     public float Y;
     public float lattice_constant;
+    
+    public float offset_x = 0;
+    public float offset_y = 0;
 
     public Transform Origin;
 
@@ -18,10 +22,23 @@ public class Grid_Generator : MonoBehaviour
     {
         for (float y = 0; y<Y; y++)
         {
+            if(y%2 == 1)
+            {
+                offset_x = 2.5f;
+                //hex assym:
+                X = X-1;
+                //hex sym:
+                //X = AltX;
+            }
+            else
+            {
+                offset_x = 0.0f;
+                X = X;
+            }
             for (float x = 0; x < X; x++)
             {
-                Vector3 pos = new Vector3(  Origin.position.x + x * lattice_constant,
-                                            Origin.position.y + y * lattice_constant, 
+                Vector3 pos = new Vector3(  Origin.position.x + x * lattice_constant + offset_x,
+                                            Origin.position.y + y * lattice_constant + offset_y, 
                                             Origin.position.z
                                         );
 
